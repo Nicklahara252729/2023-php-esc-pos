a4866ad15b0a193f15d6504d745e5c7d9c9a9c12
laharanick <laharanick@gmail.com>
2023-06-20 10:45:35 +0700

    fix confing print

--- escpos/test-print.php
+++ escpos/test-print.php
@@ -11,35 +11,38 @@ try {
      * Printer Harus Dishare
      * Nama Printer Contoh: Generic
      */
-    $connector = new WindowsPrintConnector("smb://LAPTOP-Q8K0EB15/POS80");
+    $connector = new WindowsPrintConnector("POS80");
     $printer = new Printer($connector);
 
     /** RATA TENGAH */
+    $printer->setJustification(Printer::JUSTIFY_CENTER);
     $img = EscposImage::load("logo.png",false);
     $printer->bitImage($img);
-    $title = "TEST PRINTER ANTRIAN";
     $printer->initialize();
-    $printer->setFont(Printer::FONT_A);
-    $printer->setJustification(Printer::JUSTIFY_CENTER);
     $printer->text("\n");
+
+    $printer->initialize();
+    $printer->setFont(Printer::FONT_C);
+    $printer->setTextSize(2,2);
+    $printer->setJustification(Printer::JUSTIFY_CENTER);
+    $printer->text("BAPENDA LABUSEL"."\n\n");
 
     $printer->initialize();
     $printer->setFont(Printer::FONT_B);
     $printer->setJustification(Printer::JUSTIFY_CENTER);
-    $printer->text(date('d/m/Y H:i:s') . "\n");
-    $printer->setLineSpacing(5);
+    $printer->text(date('d/m/Y H:i:s'));
     $printer->text("\n");
 
     $printer->initialize();
     $printer->setFont(Printer::FONT_A);
     $printer->setJustification(Printer::JUSTIFY_CENTER);
-    $printer->text("Nomor Antrian Anda Adalah :\n");
+    $printer->text("Nomor SPTPD Anda Adalah :\n");
     $printer->text("\n");
 
     $printer->initialize();
     $printer->setJustification(Printer::JUSTIFY_CENTER);
     $printer->setTextSize(6, 4);
-    $printer->text("A010" . "\n");
+    $printer->text("1.1-12.14.000000002" . "\n");
     $printer->text("\n");
 
     $printer->initialize();
